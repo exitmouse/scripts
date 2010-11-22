exitmouse/scripts

commit dd676e44e34fa08111d421e494ac069d6179f9fc

Make whoisd resilient against LDAP service interruptions
Fixes Trac #146


git-svn-id: file:///mit/scripts/svn/trunk@1741 db9d59ff-b01e-0410-9b1a-cd9a8c06840f

server/common/oursrc/whoisd/whoisd.tac
@@ -64,9 +64,10 @@ class WhoisFactory(protocol.ServerFactory):
 #        else:
 #            return vhost + ".mit.edu"
     def searchLDAP(self, vhost):
-        results = self.ldap.search_s(self.ldap_base, ldap.SCOPE_SUBTREE,
+        results = self.ldap.search_st(self.ldap_base, ldap.SCOPE_SUBTREE,
             ldap.filter.filter_format(
-                '(|(apacheServername=%s)(apacheServerAlias=%s))', (vhost,)*2))
+                '(|(apacheServername=%s)(apacheServerAlias=%s))', (vhost,)*2),
+                timeout=5)
         if len(results) >= 1:
             result = results[0]
             attrs = result[1]
@@ -83,11 +84,19 @@ class WhoisFactory(protocol.ServerFactory):
     def getWhois(self, vhost):
         vhost = self.canonicalize(vhost)
         info = self.vhosts.get(vhost)
-        if not info:
-            info = self.searchLDAP(vhost)
+        tries = 0
+        while (tries < 3) and not info:
+            tries += 1
+            try:
+                info = self.searchLDAP(vhost)
+            except (ldap.TIMEOUT, ldap.SERVER_DOWN):
+                self.ldap.unbind()
+                self.ldap = ldap.initialize(self.ldap_URL)
         if info:
             ret = "Hostname: %s\nAlias: %s\nLocker: %s\nDocument Root: %s" % \
                 (info['apacheServerName'], vhost, info['locker'], info['apacheDocumentRoot'])
+        elif tries == 3:
+            ret = "The whois server is experiencing problems looking up LDAP records.\nPlease contact scripts@mit.edu for help if this problem persists."
         else:
             ret = "No such hostname"
         return defer.succeed(ret)

commit 59e2579dce23e8bc6bce59bfd141da95a9ec0bb9

Fix whoisd bug introduced in r1741
The code in r1741 would cause the message about having trouble
looking up LDAP records to be returned for nonexistent host
records as well as for actual LDAP problems.  If we get an empty
search result back, but don't experience an error, we're done
and don't need to keep trying.


git-svn-id: file:///mit/scripts/svn/trunk@1742 db9d59ff-b01e-0410-9b1a-cd9a8c06840f

server/common/oursrc/whoisd/whoisd.tac
@@ -89,6 +89,7 @@ class WhoisFactory(protocol.ServerFactory):
             tries += 1
             try:
                 info = self.searchLDAP(vhost)
+                break
             except (ldap.TIMEOUT, ldap.SERVER_DOWN):
                 self.ldap.unbind()
                 self.ldap = ldap.initialize(self.ldap_URL)